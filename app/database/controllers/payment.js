89cfd0c6cd82fb43e6b48bb73e92d8c9ed968a50
matin <matinvsc@gmail.com>
2025-12-12 22:19:19 +0330

    create payment schema db and controller

---
+++ app/database/controllers/payment.js
@@ -0,0 +1,30 @@
+import Payment from "../models/payment.js";
+
+const CreatePayment = async ({ id, type, count, wallet, network, amount, trackId }) => {
+    const transactionId = new Date().getTime().toString();
+
+    await Payment.create({
+        transactionId,
+        userId: id,
+        type,
+        count,
+        wallet,
+        network,
+        amount,
+        trackId
+    });
+};
+
+const FindPayment = async (object) => {
+    return Payment.findOne(object);
+};
+
+const FindAllPayment = async (object) => {
+    return Payment.find(object);
+};
+
+const UpdatePayment = async (object2, object) => {
+    return Payment.updateOne(object2, object);
+};
+
+export { CreatePayment, FindPayment, FindAllPayment, UpdatePayment };
---
+++ app/database/models/payment.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const paymentSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    count: {
+        type: Number,
+        default: true
+    },
+    wallet: {
+        type: String,
+        required: true,
+    },
+    network: {
+        type: String,
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: false,
+    },
+    status: {
+        type: Boolean,
+        default: false
+    },
+    // 1 => true // 0 => false // 2 => cancel
+    despot: {
+        type: Number,
+        default: 0
+    },
+});
+
+const Payments = mongoose.model("payment", paymentSchema);
+
+export default Payments;
