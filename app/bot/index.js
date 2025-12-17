fd8c4e0d2e5086d409ee854a127f5797c64855ec
matin <matinvsc@gmail.com>
2025-12-17 18:55:08 +0330

    final configuration of bot

--- app/bot/index.js
+++ app/bot/index.js
@@ -6,15 +6,21 @@ import { Mongo } from "@telegraf/session/mongodb";
 // middleware
 import Command from "./middleware/command.js";
 import Action from "./middleware/action.js";
-import keyboard from "./middleware/keyboard.js";
+import Keyboard from "./middleware/keyboard.js";
+import Session from "./middleware/session.js";
 // limiter
 import { limit } from "@grammyjs/ratelimiter";
 
 
 const bot = new Telegraf(process.env.TOKEN);
 
-const Startbot = () => {
+const i18nL = new TelegrafI18n({
+    directory: "locales",
+    sessionName: "session",
+    useSession: true,
+});
 
+const Startbot = () => {
     bot.use(
         limit({
             timeFrame: 1000,
@@ -25,12 +31,6 @@ const Startbot = () => {
         })
     );
 
-    const i18n = new TelegrafI18n({
-        directory: "locales",
-        sessionName: "session",
-        useSession: true,
-    });
-
     const store = Mongo({
         url: process.env.DATABASE,
         database: "MrExchange",
@@ -39,7 +39,7 @@ const Startbot = () => {
     bot.use(session({ store, defaultSession: (ctx) => ({ __language_code: ctx.from.language_code }) }));
 
     // middleware i18n
-    bot.use(i18n.middleware());
+    bot.use(i18nL.middleware());
 
     // middleware command
     bot.command(/.*/, Command);
@@ -47,10 +47,13 @@ const Startbot = () => {
     // middleware action
     bot.on("callback_query", Action);
 
-    // middleware chatgpt api
-    bot.on(message("text"), keyboard);
+    // middleware keyboard
+    bot.on(message("text"), Keyboard);
+
+    // middleware session
+    bot.on([message("text"), message("forward_origin")], Session);
 
     bot.launch();
 };
 
-export { Startbot, bot };
+export { Startbot, bot, i18nL };
--- index.js
+++ index.js
@@ -2,8 +2,14 @@ import 'dotenv/config';
 import express from "express";
 import { Startbot } from "./app/bot/index.js";
 // database
-// import db from './app/database/connect.js';
 import connectDB from './app/database/connect.js';
+// router
+import router from './app/router/index.js';
+// cron job
+import cron from 'node-cron';
+import withdrawFailed from './app/cronjobs/withdraw.js';
+import updatePriceCrypto from './app/cronjobs/updatePriceCrypto.js';
+import sendAll from './app/cronjobs/sendAll.js';
 
 const init = async () => {
     // create server by express
@@ -14,6 +20,8 @@ const init = async () => {
         app.listen(port, () => {
             console.log(`server running : http://localhost:${port}`);
         });
+
+        app.use(router);
     };
 
     // setup db
@@ -21,8 +29,18 @@ const init = async () => {
         connectDB();
     };
 
+    // cron job
+    const Cronjobs = () => {
+        cron.schedule('* * * * *', () => {
+            withdrawFailed();
+            updatePriceCrypto();
+            sendAll();
+        });
+    };
+
     await CreateServer();
     await setupDb();
+    await Cronjobs();
     Startbot();
 };
 
